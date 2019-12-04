e60c4dd66e2560f8f7f0ed318d3aa1dbeae1642f
Robert Schneider <robmschn@gmail.com>
2019-12-04 16:42:57 -0500

    y will update twice if 3 and 5 both factors

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,9 @@
 for x in range(1,101):
-    if x % 15 == 0:
-        y = 'FizzBuzz'
-    elif x % 3 == 0:
-        y = 'Fizz'
-    elif x % 5 == 0:
-        y = 'Buzz'
-    else:
+    y = ''
+    if x % 3 == 0:
+        y += 'Fizz'
+    if x % 5 == 0:
+        y += 'Buzz'
+    if y == '':
         y = x
-    print(f'I cannot believe {x} is actually {y}!')
+    print(f'Wow! The number {x} is actually {y}!')
