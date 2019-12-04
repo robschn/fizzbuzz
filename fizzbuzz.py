57fc586c97ece7050abc41e95f1836cdf16c8b4f
Robert Schneider <robmschn@gmail.com>
2019-12-04 16:10:16 -0500

    initial upload

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+for x in range(1,101):
+    y = ''
+    if x % 15 == 0:
+        y = 'FizzBuzz'
+    elif x % 3 == 0:
+        y = 'Fizz'
+    elif x % 5 == 0:
+        y = 'Buzz'
+    else:
+        y = x
+    print(f'I cannot believe {x} is actually {y}!')
