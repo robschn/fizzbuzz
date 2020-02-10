57f5023ee171d810ecba44415f1bf46171138d0b
Robert Schneider <robmschn@gmail.com>
2020-02-10 15:25:31 -0500

    added functions!

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,16 @@
-for x in range(1,101):
-    y = ''
-    if x % 3 == 0:
-        y += 'Fizz'
-    if x % 5 == 0:
-        y += 'Buzz'
-    elif y == '':
-        y = x
-    print(f'Wow! The number {x} is actually {y}!')
+def fizz_buzz(fizz_num, fizz_tag, buzz_num, buzz_tag):    
+    for x in range(1,101):
+        y = ''
+        if x % fizz_num == 0:
+            y += fizz_tag
+        if x % buzz_num == 0:
+            y += buzz_tag
+        elif y == '':
+            y = x
+        print(f'Wow! The number {x} is actually {y}!')
+
+def main():
+    fizz_buzz(3, 'Fizz', 5, 'Buzz')
+
+if __name__ == "__main__":
+    main()
